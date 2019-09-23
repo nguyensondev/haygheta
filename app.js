ac30e8aee2f801e8cad10e5f21a92415a723c96e
Sơn Nguyễn <nguyensonrc@gmail.com>
2019-09-23 16:29:59 +0700

    check lan 2

--- app.js
+++ app.js
@@ -93,10 +93,10 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500).render('error');
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 module.exports = app;
